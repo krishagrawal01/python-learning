b9d56396107ddfe254b07f940dfa89e56213b586
01krishagrawal <01krishagrawal@gmail.com>
2026-06-23 12:58:18 +0530

    Added more functions

---
+++ enumerate.py
@@ -0,0 +1,4 @@
+fruits = ["apple", "banana", "guava", "orange", "mango"]
+
+for index, fruit in enumerate(fruits, start = 1):
+    print(index, fruit) 
---
+++ short-if-else.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a number:"))
+b = int(input("Enter another number:"))
+
+maxNum = a if a >= b else b
+
+print("Greater Number is:", maxNum)
