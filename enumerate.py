3550e4dcfcbaaaca7bd33c488deb37fd0c6904e4
01krishagrawal <01krishagrawal@gmail.com>
2026-06-24 22:41:44 +0530

    Edited a file

--- enumerate.py
+++ enumerate.py
@@ -1,3 +1,11 @@
+# fruits = ["apple", "banana", "guava", "orange", "mango"]
+
+# index = 0
+
+# for fruit in (fruits):
+#     print(index, fruit)
+#     index += 1
+
 fruits = ["apple", "banana", "guava", "orange", "mango"]
 
 for index, fruit in enumerate(fruits, start = 1):
